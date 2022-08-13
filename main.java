e4b86e1b10af974e061e6a2c9f122ba5342dad1d
DanielRasho <98480883+DanielRasho@users.noreply.github.com>
2022-08-13 15:13:10 -0600

    Initial commit

---
+++ main.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class main extends World
+{
+
+    /**
+     * Constructor for objects of class main.
+     * 
+     */
+    public main()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(700, 700, 1); 
+    }
+}
